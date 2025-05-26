340841e8798a6c498da3a8b8b269404a84635c6d
Mohammed tareq <Mohtareq1999m@gmail.com>
2025-05-27 01:58:43 +0300

    handel firebaseJs

--- assets/js/shop-details-page/firebase.js
+++ assets/js/shop-details-page/firebase.js
@@ -1,11 +1,49 @@
-// Firebase Core
+//
+//
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+// import {
+//   getAnalytics,
+//   getFirestore,
+//   collection,
+//   getDocs,
+//   doc,
+//   getDoc,
+//   limit,
+//   query,
+//   where,
+//   startAfter,
+//   setDoc,
+//   orderBy,
+//   getCountFromServer
+// } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
+// import { addDoc } from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js';
+//
+//
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBjRfUhrD8izFBizJxYvGS4PPoLjV5x6Mc",
+//   authDomain: "iti-e2.firebaseapp.com",
+//   projectId: "iti-e2",
+//   storageBucket: "iti-e2.firebasestorage.app",
+//   messagingSenderId: "394747800497",
+//   appId: "1:394747800497:web:46ffd532adfab5d7aa2b90"
+// };
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+//
+//
+//
+// // Initialize Cloud Firestore and get a reference to the service
+// const db = getFirestore(app);
+//
+// export { db, collection, addDoc, getDocs, limit, query, startAfter, doc, getDoc, where, setDoc, orderBy, app, getCountFromServer }
+//
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-
-// Firebase Firestore
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
 import {
   getFirestore,
   collection,
-  addDoc,
   getDocs,
   doc,
   getDoc,
@@ -15,10 +53,10 @@ import {
   startAfter,
   setDoc,
   orderBy,
-  getCountFromServer
+  getCountFromServer,
+  addDoc
 } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBjRfUhrD8izFBizJxYvGS4PPoLjV5x6Mc",
   authDomain: "iti-e2.firebaseapp.com",
@@ -30,11 +68,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firestore
 const db = getFirestore(app);
 
-// Export needed functions
 export {
   db,
   collection,
@@ -49,6 +87,5 @@ export {
   setDoc,
   orderBy,
   app,
-  getCountFromServer,
-  firebaseConfig
+  getCountFromServer
 };
