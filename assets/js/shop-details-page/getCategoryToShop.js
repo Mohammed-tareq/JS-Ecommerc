73fb11bab44a11780b1adef2fd650e18f51db3e4
karim <karimnase11@gmail.com>
2025-05-26 10:44:54 +0300

    remove scroll top

--- assets/js/shop-details-page/getCategoryToShop.js
+++ assets/js/shop-details-page/getCategoryToShop.js
@@ -1,31 +1,11 @@
-<<<<<<< HEAD
 import { fetchOnePage } from "./shop.js";
 
 function getCategory() {
     let category = window.location.href.split('?')[1].split('=')[1];
     if (category) {
         fetchOnePage(category);
-        // console.log(category);
-=======
-const categories = document.getElementsByClassName('category-title');
-
-
-function getCategory() {
-    for (let i = 0; i < categories.length; i++) {
-        categories[i].addEventListener('click', function (e) {
-            let category = e.target.id;
-            window.location.href = `shop.html?category=${category}`;
-        })
->>>>>>> karim
     }
 }
 
 
-<<<<<<< HEAD
-
-
 getCategory()
-
-=======
-getCategory()
->>>>>>> karim
